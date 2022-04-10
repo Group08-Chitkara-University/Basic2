a925a174ce40c9998f8f8b221cf1269fd1bc682e
Divyam Manchanda <divyammanchanda89@yahoo.com>
2022-04-10 12:57:40 +0530

    First Commit

---
+++ basic1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	/* code */
+	int num1;
+	int num2;
+	int sum;
+	cout<<"enter the first number :";
+	cin>>num1;
+	cout<<"enter second number:";
+	cin>>num2;
+	sum= num1+num2;
+	cout<<"The sum is :"<<sum;
+	
+	return 0;
+}
